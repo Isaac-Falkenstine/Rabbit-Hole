687e51c0704921d5a9d2e82795f4f07b26f0818a
Isaac Falkenstine <isaac@falkenstine.com>
2018-12-18 20:16:56 -0700

    Added data to the seed file

---
+++ spec/features/visitors/visitor_can_visit_welcome_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature "visitors go to the welcome page" do
+  scenario "and see login/register buttons" do
+    visit "/"
+
+    expect(page).to have_content("Login")
+    expect(page).to have_content("Register")
+    expect(page).to have_content("Register With Google")
+    expect(page).to have_content("you will never know less than you know right now.")
+  end
+end
