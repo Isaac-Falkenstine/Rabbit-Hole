d26456f1b52fdbc0f4a3785fd4a65e026a8dbb51
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-12-18 21:58:55 -0700

    10_google_oauth_master
    Creates oauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+<<<<<<< HEAD
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
   get '/dashboard', to: 'users#show'
+=======
+
+  get "auth/:provider/callback", to: "google_oauth#update"
+  get "auth/failure", to: redirect("/")
+
+>>>>>>> 10_google_oauth_master
 end
