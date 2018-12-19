a3e1b706a5172997a833427ff530da091a52654f
Emilio <thecraftedgem@tuta.io>
2018-12-19 16:57:33 -0700

    buttons on welcome page linked to paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :welcome, only:[:index]
-  root "welcome#index"
+  root 'welcome#index'
+
+  get "auth/:provider/callback", to: "google_oauth#update"
+  get "auth/failure", to: redirect("/")
+
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+
+  get '/register', to: "users#new"
+  get '/dashboard', to: "users#show"
+
+  resources :users, only: [:create]
+
 end
