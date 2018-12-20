1767a7c40794d12b70ba31dc132a885f24366f54
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-12-19 17:53:03 -0700

    wip

--- app/controllers/google_oauth_controller.rb
+++ app/controllers/google_oauth_controller.rb
@@ -4,17 +4,17 @@ class GoogleOauthController < ApplicationController
 
   def update
     data = request.env["omniauth.auth"]
+    refresh_token = data.credentials.refresh_token
     user = User.from_omniauth(data)
     if user
       session[:user_id] = user.id
+      user.google_oauth_token.update(refresh_token: refresh_token) if refresh_token.present?
       flash[:notice] = "You have successfully logged in!"
       redirect_to dashboard_path
     elsif user.save
-      flash[:notice] = "Account Created!"
-      GoogleOauthToken.create(token: data.credentials.token, user: user)
-      refresh_token = data.credentials.refresh_token
-      user.google_oauth_token.update(refresh_token: refresh_token) if refresh_token.present?
+      GoogleOauthToken.create(token: data.credentials.token, user: user, refresh_token: refresh_token)
       session[:user_id] = user.id
+      flash[:notice] = "Account Created!"
       redirect_to dashboard_path
     else
       flash[:notice] = "Google Authorization was unable to be completed."
