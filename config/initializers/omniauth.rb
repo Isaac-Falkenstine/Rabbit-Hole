e058e3f5f22ac1ec0d17d09512d49612f15a1c18
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-12-20 16:55:08 -0700

    Deployment fix - changes scope of google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     :name => "google_oauth2",
-    :scope => ['contacts','plus.login','plus.me','email','profile'],
+    :scope => ['email','profile','openid'],
     :prompt => "select_account",
     :access_type => 'offline'
   }
