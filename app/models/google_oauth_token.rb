217cfa297ffd67c360e72b0f0f2b52713495b984
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-12-19 20:22:54 -0700

    saves file

--- app/models/google_oauth_token.rb
+++ app/models/google_oauth_token.rb
@@ -1,7 +1,7 @@
 class GoogleOauthToken < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :token
+  validates_presence_of :tokenx
                         :refresh_token
 
   validates :user_id, uniqueness: true
