466390f782552ae443c2f400f5bbd06c94620352
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-12-20 15:38:54 -0700

    uncomments the logout path

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
-  # delete '/logout', to: "sessions#destroy"
+  delete '/logout', to: "sessions#destroy"
 
   get '/register', to: "users#new"
   post '/register', to: "users#create"
