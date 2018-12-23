ce4b5d93c642d81d2aa7c11ee195cf1a1e814fe6
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-12-23 10:48:38 -0700

    Error getting logout route to work

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module RabbitHole
     # Don't generate system test files.
     config.generators.system_tests = nil
     # config.assets.initialize_on_precompile = false
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
